17a54b393394c14ab8f4dd67e58450937779d494
Токарев Владимир ТКИ-112 <79909615+AinsFeelodin@users.noreply.github.com>
2021-04-29 09:45:00 +0300

    Add files via upload

---
+++ 1-3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+/**
+* \brief Функция, рассчитывающая общее сопротивление
+* \param R1 - сопротивление 1 резистора
+* \param R2 - сопротивление 2 резистора
+* \param R3 - сопротивление 3 резистора
+* \return значение общего сопротивления
+**/
+
+
+/**
+* \brief Точка входа в программу
+* \return Код ошибки (0-успех)
+* */
+int main() {
+    double r1, r2, r3;
+    std::cout << "VVedite R1 "; std::cin >> r1;
+    std::cout << "VVedite R2 "; std::cin >> r2;
+    std::cout << "VVedite R3 "; std::cin >> r3;
+    std::cout << "Soprotivleniу ravno " << 1 / (1 / r1 + 1 / r2 + 1 / r3) << std::endl;
+	return 0;
+}
+
+
+
